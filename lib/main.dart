593622b99439ce029fba4a0198222dd6ba96d9e0
Lucas Reis <lucasreis_9@hotmail.com>
2023-05-15 17:41:46 -0300

    Criada a interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:on_audio_query/on_audio_query.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +13,69 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      title: 'Music Player',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      // theme: ThemeData(
+      //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      //   useMaterial3: true,
+      // ),
+      home: AllSongs(),
+    );
+  }
+}
+
+class AllSongs extends StatefulWidget {
+  const AllSongs({super.key});
+
+  @override
+  State<AllSongs> createState() => _AllSongsState();
+}
+
+class _AllSongsState extends State<AllSongs> {
+  final _audioQuery = OnAudioQuery();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Musica App'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+        ],
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      body: FutureBuilder<List<SongModel>>(
+        future: _audioQuery.querySongs(
+          sortType: null,
+          orderType: OrderType.ASC_OR_SMALLER,
+          uriType: UriType.EXTERNAL,
+          ignoreCase: true,
+        ),
+        builder: (context, item) {
+          if (item.data == null) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (item.data!.isEmpty) {
+            return Center(
+              child: Text('No Songs found'),
+            );
+          }
+          return ListView.builder(
+            itemCount: 100,
+            itemBuilder: (context, index) => ListTile(
+              leading: Icon(Icons.music_note),
+              title: Text(item.data![index].displayNameWOExt),
+              subtitle: Text('${item.data![index].artist}'),
+              trailing: Icon(Icons.more_vert),
+            ),
+          );
+        },
+      ),
     );
   }
 }
